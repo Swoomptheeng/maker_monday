36288375cf1699cf2f81019870a3ba93cfcb6705
Ben Neal <info@psiconlab.co.uk>
2016-09-11 13:29:56 +0100

    Flex sensor added

---
+++ Flex_Sensor_with_MIDI.ino
@@ -0,0 +1,58 @@
+/******************************************************************************
+Flex_Sensor_Example.ino
+Example sketch for SparkFun's flex sensors
+  (https://www.sparkfun.com/products/10264)
+Jim Lindblom @ SparkFun Electronics
+April 28, 2016
+
+Create a voltage divider circuit combining a flex sensor with a 47k resistor.
+- The resistor should connect from A5 to GND.
+- The flex sensor should connect from A5 to 5V
+As the resistance of the flex sensor increases (meaning it's being bent), the
+voltage at A5 should decrease.
+
+Development environment specifics:
+Arduino 1.6.7
+******************************************************************************/
+const int FLEX_PIN = A5; // Pin connected to voltage divider output
+
+// Measure the voltage at 5V and the actual resistance of your
+// 47k resistor, and enter them below:
+const float VCC = 4.98; // Measured voltage of Ardunio 5V line
+const float R_DIV = 47500.0; // Measured resistance of 3.3k resistor
+
+// Upload the code, then try to adjust these values to more
+// accurately calculate bend degree.
+const float STRAIGHT_RESISTANCE = 37300.0; // resistance when straight
+const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
+
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(FLEX_PIN, INPUT);
+}
+
+void loop() 
+{
+  // Read the ADC, and calculate voltage and resistance from it
+  int flexADC = analogRead(FLEX_PIN);
+  float flexV = flexADC * VCC / 1023.0;
+  float flexR = R_DIV * (VCC / flexV - 1.0);
+  //Serial.println("Resistance: " + String(flexR) + " ohms");
+
+  // Use the calculated resistance to estimate the sensor's
+  // bend angle:
+  float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
+                   0, 90.0);
+  //Serial.println("Bend: " + String(angle) + " degrees");
+  //Serial.println();
+  
+  //MIDI
+  angle = constrain(angle, 50.0, 70.0);
+  int val = map((int) angle, 50, 70, 0, 127);    //50 here is natural "unsqueezed" angle and 70 full compression
+  Serial.write(0xB3); //CC & Channel Num
+  Serial.write(0x01); //Controller Num
+  Serial.write(val); // Value
+
+  delay(150);
+}
